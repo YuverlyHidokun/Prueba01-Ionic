dc09299e218c85f1dfe1139f862e01b595625e30
YuverlyHidokun <yuverlyverdezoto@gmail.com>
2024-10-25 14:42:05 -0500

    tab 4 raices comppleto

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -3,10 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab4',
   templateUrl: 'tab4.page.html',
-  styleUrls: ['tab4.page.scss']
+  styleUrls: ['tab4.page.scss'],
 })
 export class Tab4Page {
+  a: number | null = null;
+  b: number | null = null;
+  c: number | null = null;
+  raiz1: number | null = null;
+  raiz2: number | null = null;
+  error: string = '';
 
-  constructor() {}
+  calcularRaices() {
+    this.error = '';
+    this.raiz1 = null;
+    this.raiz2 = null;
 
+    if (this.a === null || this.b === null || this.c === null) {
+      this.error = 'Por favor, ingresa valores válidos para a, b y c.';
+      return;
+    }
+
+    if (this.a === 0) {
+      this.error = 'El coeficiente "a" no puede ser cero.';
+      return;
+    }
+
+    const discriminante = this.b * this.b - 4 * this.a * this.c;
+
+    if (discriminante < 0) {
+      this.error = 'Las raíces son complejas.';
+    } else {
+      this.raiz1 = (-this.b + Math.sqrt(discriminante)) / (2 * this.a);
+      this.raiz2 = (-this.b - Math.sqrt(discriminante)) / (2 * this.a);
+    }
+  }
 }
